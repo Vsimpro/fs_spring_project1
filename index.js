6b4cb71cdb2b7c9883762b6eca56b1a64a604deb
Vsimpro <jsimrpo@gmail.com>
2023-02-21 12:10:40 +0200

    add index.html

--- index.js
+++ index.js
@@ -1,15 +1,67 @@
+// Libraries
+const fs = require("fs");
 const express = require("express");
 
 
 // Global variables:
-const PORT = 8080
-const HOST = "localhost"
-const app = express()
+const app = express();
+const PORT = 8080;
+const HOST = "localhost";
+
+const ERROR_RESPONSES = {
+    "404" : "<strong>404</strong><br>Sorry (ツ)",
+    "500" : "<strong>500</strong> INTERNAL SERVER ERROR."
+};
+
+const ROUTES = {
+    "/"           : index,
+    "index"       : index,
+    "/index.html" : index,
+
+    /* TODO: Add functions for these routes.
+    "/guestbook"       : guestbook(),
+    "/guestbook.html"  : guestbook(),  
+
+    "/newmessage"      : new_message(),
+    "/newmessage.html" : new_message(),
+
+    "/ajaxmessage"       : ajax_message(),
+    "/ajaxmessage.html"  : ajax_message(),
+    */
+};
+
+
+// Route Functions:
+function index(request) {
+    var response_text = ERROR_RESPONSES["500"]
+
+    try {
+        response_text = fs.readFileSync("templates/index.html", "utf8") 
+    } catch (error) {
+        if (error.code === "ENOENT") {
+            response_text = ERROR_RESPONSES["404"]
+        } 
+        console.log("!!! " + error);
+    }
+
+    return response_text
+}
 
 
 // Routing:
-app.get("/", function(request, response) {
-    response.send("Hello World!")
+app.get("/:route", function(request, response) {
+    let msg = "!!! Error: " + request
+    let http_response = ERROR_RESPONSES["404"] // TODO: Standardise these from a dic.
+    let route = `${request.params.route}`;
+    
+    if (route in ROUTES) {
+        msg = "> GET '" + route + "'";
+        http_response = ROUTES[ route ](request);
+    }
+
+    console.log(msg);
+    response.send(http_response);
+    return; 
 });
 
 
