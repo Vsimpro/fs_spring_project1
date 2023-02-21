26c4193d2816429b0b5ef0a47419d1ef8d7ccf09
Vsimpro <jsimrpo@gmail.com>
2023-02-21 12:45:33 +0200

    add guestbook poc

--- index.js
+++ index.js
@@ -14,13 +14,13 @@ const ERROR_RESPONSES = {
 };
 
 const ROUTES = {
-    "/"           : index,
     "index"       : index,
-    "/index.html" : index,
+    "index.html"  : index,
+
+    "guestbook"       : guestbook,
+    "guestbook.html"  : guestbook,  
 
     /* TODO: Add functions for these routes.
-    "/guestbook"       : guestbook(),
-    "/guestbook.html"  : guestbook(),  
 
     "/newmessage"      : new_message(),
     "/newmessage.html" : new_message(),
@@ -30,27 +30,81 @@ const ROUTES = {
     */
 };
 
+/* Functionality Functions. */
+// json_into_html : parses JSON given as a parameter into HTML tables.
+function json_into_html(data) {
+    let json_data = JSON.parse(data)
 
-// Route Functions:
+    let html_generated = "<table border='1'>"
+    html_generated += `
+        <tr><th> Username. </th>
+            <th> Message.  </th></tr>\n`
+
+    for (let i = 0; i < json_data.length; i++) {
+        let book_insert = json_data[i];
+
+        let insert_id       = book_insert["id"]      || "null";
+        let insert_msg      = book_insert["message"] || "null";
+        let insert_date     = book_insert["date"]    || "null";
+        let insert_uname    = book_insert["username"]|| "null";
+        let insert_country  = book_insert["country"] || "null";
+        
+        // TODO: Template rest of data into the table.
+        html_generated += `
+        <tr><th> ${insert_uname} </th>
+            <th> ${insert_msg}   </th></tr>\n`
+    }
+    html_generated += "</table>"
+    return html_generated
+};
+
+/* Route Functions: */
+// index : returns index.html
 function index(request) {
     var response_text = ERROR_RESPONSES["500"]
 
     try {
         response_text = fs.readFileSync("templates/index.html", "utf8") 
+
     } catch (error) {
         if (error.code === "ENOENT") {
             response_text = ERROR_RESPONSES["404"]
         } 
+        
         console.log("!!! " + error);
     }
 
     return response_text
-}
+};
+
+// guestbook : returns guestbook.html
+function guestbook() {
+    var response_text = ERROR_RESPONSES["500"]
+    try {
+        data = fs.readFileSync("data/sample.json", "utf8") 
+        if (data == undefined | data == null) 
+            { throw error; }
+
+        converted_json = json_into_html(data)
+        response_text = converted_json
+
+    } catch (error) {
+        console.log("!!! Error: While parsing guestbook. Details\n" + error)
+    }
+
+    return response_text
+
+};
 
 
 // Routing:
+app.get("/", function(request, response) {
+    response.send(index()); console.log("> GET '/'");
+    return; 
+});
+
 app.get("/:route", function(request, response) {
-    let msg = "!!! Error: " + request
+    let msg = "!!! Error 404: " + request.params.route
     let http_response = ERROR_RESPONSES["404"] // TODO: Standardise these from a dic.
     let route = `${request.params.route}`;
     
